286af1514be32f4975b1e123f492c5d7e5b2200a
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-27 02:03:50 +0300

    v1: adding 2 array list and using old way for each

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Ahmed");
+        names.add("Ayman");
+        names.add("Abdelfatah");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(20);
+        numbers.add(50);
+        numbers.add(90);
+        System.out.println("names: old way");
+        for (String name : names) {
+            System.out.println(name);
+        }
+        System.out.println("-------------------------------");
+        System.out.println("numbers: old way");
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
     }
 }
